1aa69d0fb8497d3dfc8bd44c6715bfcb7627525a
Monica Orme <70462622+Monica-ro@users.noreply.github.com>
2021-07-21 22:33:17 -0700

    Add files via upload
    This is a calendar model class that follows the MVC pattern. Maybe we could rename the original CalendarModel class to something else (such as CalendarViewer) and use this class as the model?

--- CalendarModel.java
+++ CalendarModel.java
@@ -1,13 +1,12 @@
-import java.time.LocalDate;
+package project;
+
 /**
- * @author Nick Fong, Monica Orme, Ben Gonzalez
+ * @author  Monica Orme
  *
  */
-import java.util.ArrayList;
-import java.util.TreeSet;
 
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
+import java.util.*;
+import javax.swing.event.*;
 
 /**
 An MVC based model for the Calendar application
@@ -15,100 +14,43 @@ An MVC based model for the Calendar application
 
 
 public class CalendarModel {
-    private LocalDate currentDate;
-    //private TreeMap<LocalDate, ArrayList<Event>> events;
-//    private TreeSet<Event> events;
+   // private TreeMap<LocalDate, ArrayList<Event>> events;
+    private TreeSet<Event> events;
     private ArrayList<ChangeListener> listeners;
 
     public CalendarModel() {
- //       events = new TreeSet<>();
+        events = new TreeSet<>();
         listeners = new ArrayList<>();
     }
     
-    /**
-    Constructs the calendar model
-    @param cd - current date
-    */
-    public CalendarModel(LocalDate cd) {
-        currentDate = cd;
-    }
-
-    /**
-    [METHOD DESCRIPTION
     
-    @return  [FILL THIS IN]
-    */
-    public int getDaysInMonth() {
-        return currentDate.lengthOfMonth();
-    }
-
-    /**
-    [METHOD DESCRIPTION
-    
-    @return  [FILL THIS IN]
-    */
-    public String getMonth() {
-        return currentDate.getMonth().toString();
-    }
-
-    /**
-    [METHOD DESCRIPTION
-    
-    @return  [FILL THIS IN]
-    */
-    public int getYear() {
-        return currentDate.getYear();
-    }
-
-
-    /**
-    [METHOD DESCRIPTION
-    
-    @return  [FILL THIS IN]
-    */
-    public static int getFirstOfMonth(LocalDate date) {
-        return weekTool(date.getDayOfWeek().toString());
-    }
-
-    /**
-    [METHOD DESCRIPTION
-    
-    @return  [FILL THIS IN]
-    */
-    public static int weekTool(String day) {
-        if (day.equals("SUNDAY")) {
-            return 1;
-        }
-        else if (day.equals("MONDAY")) {
-            return 2;
-        }
-        else if (day.equals("TUESDAY")) {
-            return 3;
-        }
-        else if (day.equals("WEDNESDAY")) {
-            return 4;
-        }
-        else if (day.equals("THURSDAY")) {
-            return 5;
-        }
-        else if (day.equals("FRIDAY")) {
-            return 6;
-        }
-        else {
-            return 7;
-        }
-    }
-    
+    // accessor
     /**
     Constructs a DataModel object
-    @return the data in an ArrayList
+    @return the event data in an ArrayList
 	 */
-    /*
+    
 	 public ArrayList<Event> getData() {
-	    return (ArrayList<Event>) (event.clone());
+		return new ArrayList<>(events);
+	    //return (ArrayList<Event>) (events.clone());
 	 }
-	 */
+	 
 	
+    // mutator
+	 /**
+    Adds the event to the calendar model and notifies all change listeners
+    in the model about this update. 
+    @param e - an event
+ */
+ 
+	 public void addEvent(Event e) {
+	    //events.put(e.getDate(), add(e));
+		 events.add(e);
+	    for (ChangeListener l : listeners) {
+	       l.stateChanged(new ChangeEvent(this));
+	    }
+	 }
+    
 	 /**
 	    Attach a listener to the Model
 	    @param c - the listener
@@ -116,19 +58,6 @@ public class CalendarModel {
 	 public void attach(ChangeListener c) {
 	    listeners.add(c);
 	 }
-	
-	 /**
-	    Change the data in the model at a particular location
-	    @param location the index of the field to change
-	    @param value the new value
-	 */
-	 
-	 /*
-	 public void update(int location, double value) {
-	    data.set(location, new Double(value));
-	    for (ChangeListener l : listeners) {
-	       l.stateChanged(new ChangeEvent(this));
-	    }
-	 }
-	 */
+
 }
+
