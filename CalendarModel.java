33e5b3f9da32a855d4b043d8c56146553135d9ad
Monica Orme <70462622+Monica-ro@users.noreply.github.com>
2021-07-21 22:35:00 -0700

    Update CalendarModel.java

--- CalendarModel.java
+++ CalendarModel.java
@@ -1,12 +1,13 @@
-package project;
-
+import java.time.LocalDate;
 /**
- * @author  Monica Orme
+ * @author Nick Fong, Monica Orme, Ben Gonzalez
  *
  */
+import java.util.ArrayList;
+import java.util.TreeSet;
 
-import java.util.*;
-import javax.swing.event.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 /**
 An MVC based model for the Calendar application
@@ -14,43 +15,100 @@ An MVC based model for the Calendar application
 
 
 public class CalendarModel {
-   // private TreeMap<LocalDate, ArrayList<Event>> events;
-    private TreeSet<Event> events;
+    private LocalDate currentDate;
+    //private TreeMap<LocalDate, ArrayList<Event>> events;
+//    private TreeSet<Event> events;
     private ArrayList<ChangeListener> listeners;
 
     public CalendarModel() {
-        events = new TreeSet<>();
+ //       events = new TreeSet<>();
         listeners = new ArrayList<>();
     }
     
+    /**
+    Constructs the calendar model
+    @param cd - current date
+    */
+    public CalendarModel(LocalDate cd) {
+        currentDate = cd;
+    }
+
+    /**
+    [METHOD DESCRIPTION
     
-    // accessor
+    @return  [FILL THIS IN]
+    */
+    public int getDaysInMonth() {
+        return currentDate.lengthOfMonth();
+    }
+
+    /**
+    [METHOD DESCRIPTION
+    
+    @return  [FILL THIS IN]
+    */
+    public String getMonth() {
+        return currentDate.getMonth().toString();
+    }
+
+    /**
+    [METHOD DESCRIPTION
+    
+    @return  [FILL THIS IN]
+    */
+    public int getYear() {
+        return currentDate.getYear();
+    }
+
+
+    /**
+    [METHOD DESCRIPTION
+    
+    @return  [FILL THIS IN]
+    */
+    public static int getFirstOfMonth(LocalDate date) {
+        return weekTool(date.getDayOfWeek().toString());
+    }
+
+    /**
+    [METHOD DESCRIPTION
+    
+    @return  [FILL THIS IN]
+    */
+    public static int weekTool(String day) {
+        if (day.equals("SUNDAY")) {
+            return 1;
+        }
+        else if (day.equals("MONDAY")) {
+            return 2;
+        }
+        else if (day.equals("TUESDAY")) {
+            return 3;
+        }
+        else if (day.equals("WEDNESDAY")) {
+            return 4;
+        }
+        else if (day.equals("THURSDAY")) {
+            return 5;
+        }
+        else if (day.equals("FRIDAY")) {
+            return 6;
+        }
+        else {
+            return 7;
+        }
+    }
+    
     /**
     Constructs a DataModel object
-    @return the event data in an ArrayList
+    @return the data in an ArrayList
 	 */
-    
+    /*
 	 public ArrayList<Event> getData() {
-		return new ArrayList<>(events);
-	    //return (ArrayList<Event>) (events.clone());
+	    return (ArrayList<Event>) (event.clone());
 	 }
-	 
+	 */
 	
-    // mutator
-	 /**
-    Adds the event to the calendar model and notifies all change listeners
-    in the model about this update. 
-    @param e - an event
- */
- 
-	 public void addEvent(Event e) {
-	    //events.put(e.getDate(), add(e));
-		 events.add(e);
-	    for (ChangeListener l : listeners) {
-	       l.stateChanged(new ChangeEvent(this));
-	    }
-	 }
-    
 	 /**
 	    Attach a listener to the Model
 	    @param c - the listener
@@ -58,6 +116,19 @@ public class CalendarModel {
 	 public void attach(ChangeListener c) {
 	    listeners.add(c);
 	 }
-
+	
+	 /**
+	    Change the data in the model at a particular location
+	    @param location the index of the field to change
+	    @param value the new value
+	 */
+	 
+	 /*
+	 public void update(int location, double value) {
+	    data.set(location, new Double(value));
+	    for (ChangeListener l : listeners) {
+	       l.stateChanged(new ChangeEvent(this));
+	    }
+	 }
+	 */
 }
-
