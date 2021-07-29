3b20289e2aa7b24afc5ec48f847ceb52ff32f807
Monica O <70462622+Monica-ro@users.noreply.github.com>
2021-07-28 18:03:47 -0700

    Update DayView.java

--- DayView.java
+++ DayView.java
@@ -1,21 +1,79 @@
-import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import java.awt.*;
-import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+import java.util.*;
 
+import javax.swing.*;
+import javax.swing.event.*;
+
+/**
+ * @author Monica Orme
+ *
+ */
 public class DayView extends JPanel implements ChangeListener {
-    public DayView(CalendarModel model) {
+	private CalendarModel model;
+	private String viewMetric;
+	private LocalDate highlightedDate;
+	JTextArea events;
+	
+	
+	DayView(CalendarModel calModel) {
+		this.model = calModel;
+		this.viewMetric = "day";
+		this.highlightedDate = calModel.getHighlightedDate();
+		returnView();
+		
+	}
+	
+	public void returnView() {
+		this.removeAll();
+		this.revalidate();
+		
+		this.setLayout(new BorderLayout());
+		
+		this.setPreferredSize(new Dimension(700, 470));
+		
+		events = new JTextArea();
+		events.setText("Day View");
+		
+		this.add(events,BorderLayout.CENTER);
+        
+        /**
         this.setPreferredSize(new Dimension(700, 470));
         this.setBackground(Color.BLUE);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("E, MMM d yyyy");
         JLabel date = new JLabel("DAY VIEW  " + formatter.format(model.getHighlightedDate()).toString());
         this.add(date);
-    }
+        */
+		 
+	}
+	
+	public ArrayList<Event> getDayEvents() {
+		ArrayList<Event> dayEvents = new ArrayList<>();
+		// add events
+		for (Event e: model.getData()) {
+			if(e.getDate().equals(highlightedDate)) {
+				dayEvents.add(e);
+			}	
+		}
+		return dayEvents;
+		
+	}
+	
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		// display contents
+		// if the model's
+		// view metric matches with this one
+		if (model.getMetric().equalsIgnoreCase(viewMetric)) {
+			returnView();
+			System.out.println("This is the day view");
+		}
+		else {
+			// don't display anything
+		}
+		
+		
 
+	}
 
-    @Override
-    public void stateChanged(ChangeEvent e) {
-
-    }
 }
