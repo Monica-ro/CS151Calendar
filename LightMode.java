8846c0c22076fda6f10b8adc4ab14300347ccbc7
Monica O <70462622+Monica-ro@users.noreply.github.com>
2021-07-31 15:13:22 -0700

    Update LightMode.java

--- LightMode.java
+++ LightMode.java
@@ -10,20 +10,17 @@ import java.awt.*;
  * @author Monica Orme
  *
  */
+
+
+
+/**
+Changes the color of JPanel's background
+ *
+ */
 public class LightMode implements ColorModeStrategy {
 
     @Override
     public Color updateBackground() {
         return Color.lightGray;
     }
-
-    /*
-    @Override
-    public JPanel updatePanelTheme(JPanel original) {
-        JPanel newPanel = original;
-        newPanel.setBackground(updateBackground());
-        return newPanel;
-    }
-    */
-
 }
