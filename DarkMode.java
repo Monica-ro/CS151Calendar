52dd3a0a27387161413b62f33f6227ad65307385
Monica O <70462622+Monica-ro@users.noreply.github.com>
2021-07-31 15:12:40 -0700

    Update DarkMode.java

--- DarkMode.java
+++ DarkMode.java
@@ -5,27 +5,8 @@ import java.awt.*;
  * @author Monica Orme
  */
 public class DarkMode implements ColorModeStrategy {
-
-    //JPanel original;
-
-    public DarkMode() {
-        //this.original = originalPanel;
-    }
-
     @Override
     public Color updateBackground() {
         return Color.darkGray;
     }
-
-    /*
-    @Override
-    public JPanel updatePanelTheme(JPanel original) {
-        JPanel newPanel = original;
-        newPanel.setBackground(updateBackground());
-
-        return newPanel;
-    }
-    */
-
-
 }
