c74f695972391d675d32d377f5f8ecda3ce8651e
BenG7447 <53404904+BenG7447@users.noreply.github.com>
2021-07-30 17:33:11 -0700

    Update WeekView.java

--- WeekView.java
+++ WeekView.java
@@ -2,7 +2,9 @@ import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class WeekView extends JPanel implements ChangeListener {
@@ -23,31 +25,77 @@ public class WeekView extends JPanel implements ChangeListener {
     }
 
     public void returnView() {
-        this.removeAll();
 
+    	this.removeAll();
         this.revalidate();
-        this.setLayout(new BorderLayout());
-
-        this.setPreferredSize(new Dimension(700, 457));
         this.setBackground(Color.YELLOW);
 
-        // panel display methods
-        events = new JTextArea();
-        currentDateToDisplay = new JLabel("Current date: " + highlightedDate);
-        if (!getWeekEvents().isEmpty()) {
-            events.setText(model.format(getWeekEvents()));
-        }
-        else {
-            events.setText("No events today");
-        }
-        this.add(currentDateToDisplay, BorderLayout.NORTH);
-        this.add(events, BorderLayout.CENTER);
+        JScrollPane calendar;
+        calendar = printCalendar();
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                calendar.getVerticalScrollBar().setValue(0);
+            }
+        });
 
+        this.add(calendar);
 
-        //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("E, MMM d yyyy");
-        //JLabel date = new JLabel("WEEK VIEW  " + formatter.format(model.getHighlightedDate()).toString());
-        //this.add(date);
-
+        calendar.revalidate();
+        calendar.repaint();
+    	
+    }
+    
+    
+    public JScrollPane printCalendar() {
+    	
+    	JTextArea calendar = new JTextArea();
+    	calendar.setSelectionStart(0);
+    	calendar.setSelectionEnd(0);
+    	
+    	ArrayList<Event> events = getWeekEvents();
+    
+    	int daysFromSunday = MonthPanel.weekTool(highlightedDate.getDayOfWeek().name()) - 1;
+    	LocalDate firstDayOfWeek = highlightedDate.minusDays(daysFromSunday);
+    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("E, MMM d yyyy");
+    	
+    	ArrayList<Event> sunday = new ArrayList<Event>();
+    	ArrayList<Event> monday = new ArrayList<Event>();
+    	ArrayList<Event> tuesday = new ArrayList<Event>();
+    	ArrayList<Event> wednesday = new ArrayList<Event>();
+    	ArrayList<Event> thursday = new ArrayList<Event>();
+    	ArrayList<Event> friday = new ArrayList<Event>();
+    	ArrayList<Event> saturday = new ArrayList<Event>();
+    	
+    	for(Event e : events) {
+    		if(e.getDaysofTheWeek().equals(DayOfWeek.SUNDAY.name())) { sunday.add(e); }
+    		if(e.getDaysofTheWeek().equals(DayOfWeek.MONDAY.name())) { monday.add(e); }
+    		if(e.getDaysofTheWeek().equals(DayOfWeek.TUESDAY.name())) { tuesday.add(e); }
+    		if(e.getDaysofTheWeek().equals(DayOfWeek.WEDNESDAY.name())) { wednesday.add(e); }
+    		if(e.getDaysofTheWeek().equals(DayOfWeek.THURSDAY.name())) { thursday.add(e); }
+    		if(e.getDaysofTheWeek().equals(DayOfWeek.FRIDAY.name())) { friday.add(e); }
+    		if(e.getDaysofTheWeek().equals(DayOfWeek.SATURDAY.name())) { saturday.add(e); }
+    	}
+    	
+    	calendar.append("--------"+ formatter.format(firstDayOfWeek) + "---------" + "\n");
+    	calendar.append(model.format(sunday) + "\n");
+    	calendar.append("--------"+ formatter.format(firstDayOfWeek.plusDays(1)) + "---------" + "\n");
+    	calendar.append(model.format(monday) + "\n");
+    	calendar.append("--------"+ formatter.format(firstDayOfWeek.plusDays(2)) + "---------" + "\n");
+    	calendar.append(model.format(tuesday) + "\n");
+    	calendar.append("--------"+ formatter.format(firstDayOfWeek.plusDays(3)) + "---------" + "\n");
+    	calendar.append(model.format(wednesday) + "\n");
+    	calendar.append("--------"+ formatter.format(firstDayOfWeek.plusDays(4)) + "---------" + "\n");
+    	calendar.append(model.format(thursday) + "\n");
+    	calendar.append("--------"+ formatter.format(firstDayOfWeek.plusDays(5)) + "---------" + "\n");
+    	calendar.append(model.format(friday) + "\n");
+    	calendar.append("--------"+ formatter.format(firstDayOfWeek.plusDays(6)) + "---------" + "\n");
+    	calendar.append(model.format(saturday) + "\n");
+    	
+    	JScrollPane scroll = new JScrollPane(calendar);
+    	scroll.setPreferredSize(new Dimension(700, 449));
+    	
+    	return scroll;
+    	
     }
 
     public ArrayList<Event> getWeekEvents() {
@@ -69,7 +117,6 @@ public class WeekView extends JPanel implements ChangeListener {
             if (e.getDate().equals(sunday) || e.getDate().equals(monday) || e.getDate().equals(tuesday)
                     || e.getDate().equals(wednesday) || e.getDate().equals(thursday)
                     || e.getDate().equals(friday) || e.getDate().equals(saturday)) {
-                System.out.println("inside if of get week method");
                 weekEvents.add(e);
             }
         }
