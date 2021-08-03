22e77a33985e21b46cf7f7c1dce01da513710478
NickAFong <nickfo@rocketmail.com>
2021-08-02 18:36:47 -0700

    Update AgendaView.java

--- AgendaView.java
+++ AgendaView.java
@@ -16,18 +16,29 @@ public class AgendaView extends JPanel implements ChangeListener {
     private LocalDate localStart;
     private LocalDate localEnd;
     private JScrollPane calendar;
+    private char currentDayOfTheWeek;
+    private char[] daysOfWeekConversion = {' ', 'M', 'T', 'W', 'H', 'F', 'A', 'S'};
+    private char dow;
 
-
+    /**
+     * Constructs the agenda view in the calendar.
+     *
+     * @param calModel - The calendar model of CalendarModel type
+     */
     AgendaView(CalendarModel calModel) {
         this.model = calModel;
         this.viewMetric = "agenda";
         this.currentEventData = calModel.getData();
         this.setPreferredSize(new Dimension(700, 455));
+        this.currentEventData = calModel.getData();
 
         returnView();
 
     }
 
+    /**
+     * Updates the view panel upon construction and whenever there is an update in the model if the user has selected agenda view.
+     */
     public void returnView() {
 
         this.removeAll();
@@ -81,7 +92,11 @@ public class AgendaView extends JPanel implements ChangeListener {
 
     }
 
-
+    /**
+     * prints the agenda
+     *
+     * @return JScrollPane
+     */
     public JScrollPane printAgenda(LocalDate s, LocalDate e) {
         JTextArea agenda = new JTextArea();
         JScrollPane scroll = new JScrollPane(agenda);
@@ -89,39 +104,63 @@ public class AgendaView extends JPanel implements ChangeListener {
         agenda.setSelectionEnd(0);
 
         dateIterator = localStart;
+
         if (localStart == null || localEnd == null) {
             return scroll;
         }
         while (dateIterator.isBefore(localEnd) || dateIterator.equals(localEnd)) {
-            if (!getMonthEvents().isEmpty()) {
+            if (!getAgendaEvents().isEmpty()) {
+                dow = daysOfWeekConversion[dateIterator.getDayOfWeek().getValue()];
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("E, MMM d yyyy");
                 agenda.append("---" + formatter.format(dateIterator) + "---" + "\n");
-                agenda.append(model.format(getMonthEvents()) + "\n");
+                agenda.append(model.format(getAgendaEvents()) + "\n");
             }
             dateIterator = dateIterator.plusDays(1);
 
         }
-
-
-        agenda.append(model.format(getMonthEvents()) + "\n");
+        agenda.append(model.format(getAgendaEvents()) + "\n");
         scroll.setPreferredSize(new Dimension(700, 400));
         return scroll;
 
     }
 
-
-    public ArrayList<Event> getMonthEvents() {
+    /**
+     * Obtains the list of events that occur during a time interval
+     *
+     * @return a list of events that occur on the current date as ArrayList<Event>
+     */
+    public ArrayList<Event> getAgendaEvents() {
         ArrayList<Event> monthEvents = new ArrayList<>();
 
         // add events
         for (Event e : model.getData()) {
-            if (e.getDate().equals(dateIterator)) {
+            if (e.getDate().equals(dateIterator) && !e.getIsRecurring()) {
+                monthEvents.add(e);
+            }
+            if (e.getIsRecurring() == true && dateIterator.isAfter(e.getStartDate()) && dateIterator.isBefore(e.getEndDate()) && containsDayOfWeek(e, dow) == true
+                    || e.getIsRecurring() == true && dateIterator.equals(e.getStartDate()) && containsDayOfWeek(e, dow) == true
+                    || e.getIsRecurring() == true && dateIterator.equals(e.getEndDate()) && containsDayOfWeek(e, dow) == true) {
                 monthEvents.add(e);
             }
         }
-
         return monthEvents;
     }
+
+    /**
+     * Checks if the highlighted date's day of the week matches with the recurring event
+     *
+     * @param ev - an Event
+     * @return a boolean where true indicates that the current date's (in the model) day of the week matches with the event and false otherwise
+     */
+    public boolean containsDayOfWeek(Event ev, char dow) {
+        char[] daysOfWeekEvent = ev.getDaysofTheWeek().toCharArray();
+        for (char c : daysOfWeekEvent) {
+            if (dow == c)
+                return true;
+        }
+        return false;
+    }
+
 
     @Override
     public void stateChanged(ChangeEvent e) {
